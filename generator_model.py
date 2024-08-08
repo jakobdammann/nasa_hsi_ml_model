54ecd72971f2aa377ff9ae151cb191a6fd748d9f
j.dammann <j.dammann@stud.uni-goettingen.de>
2024-08-08 20:53:53 +0200

    out_channels of Gen can now be specified

--- generator_model.py
+++ generator_model.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 class Block(nn.Module):
     def __init__(self, in_channels, out_channels, down=True, act="relu", use_dropout=False):
         super(Block, self).__init__()
+
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False, padding_mode="reflect")
             if down
@@ -23,12 +24,14 @@ class Block(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, in_channels=3, features=64):
+    def __init__(self, in_channels=1, out_channels=3, features=64):
         super().__init__()
+
         self.initial_down = nn.Sequential(
             nn.Conv2d(in_channels, features, 4, 2, 1, padding_mode="reflect"),
             nn.LeakyReLU(0.2),
         )
+
         self.down1 = Block(features, features * 2, down=True, act="leaky", use_dropout=False)
         self.down2 = Block(features * 2, features * 4, down=True, act="leaky", use_dropout=False)
         self.down3 = Block(features * 4, features * 8, down=True, act="leaky", use_dropout=False)
@@ -45,9 +48,9 @@ class Generator(nn.Module):
         self.up5 = Block(features * 8 * 2, features * 4, down=False, act="relu", use_dropout=False)
         self.up6 = Block(features * 4 * 2, features * 2, down=False, act="relu", use_dropout=False)
         self.up7 = Block(features * 2 * 2, features, down=False, act="relu", use_dropout=False)
-        
+
         self.final_up = nn.Sequential(
-            nn.ConvTranspose2d(features * 2, in_channels, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(features * 2, out_channels, kernel_size=4, stride=2, padding=1),
             nn.Tanh(),
         )
 
@@ -60,19 +63,19 @@ class Generator(nn.Module):
         d6 = self.down5(d5)
         d7 = self.down6(d6)
         bottleneck = self.bottleneck(d7)
-        up1 = self.up1(bottleneck)
-        up2 = self.up2(torch.cat([up1, d7], 1))
-        up3 = self.up3(torch.cat([up2, d6], 1))
-        up4 = self.up4(torch.cat([up3, d5], 1))
-        up5 = self.up5(torch.cat([up4, d4], 1))
-        up6 = self.up6(torch.cat([up5, d3], 1))
-        up7 = self.up7(torch.cat([up6, d2], 1))
-        return self.final_up(torch.cat([up7, d1], 1))
+        u1 = self.up1(bottleneck)
+        u2 = self.up2(torch.cat([u1, d7], 1))
+        u3 = self.up3(torch.cat([u2, d6], 1))
+        u4 = self.up4(torch.cat([u3, d5], 1))
+        u5 = self.up5(torch.cat([u4, d4], 1))
+        u6 = self.up6(torch.cat([u5, d3], 1))
+        u7 = self.up7(torch.cat([u6, d2], 1))
+        return self.final_up(torch.cat([u7, d1], 1))
 
 
 def test():
-    x = torch.randn((1, 3, 256, 256))
-    model = Generator(in_channels=3, features=64)
+    x = torch.randn((1, 1, 256, 256))
+    model = Generator(in_channels=1, out_channels=106, features=64)
     preds = model(x)
     print(preds.shape)
 
