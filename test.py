4d0d70f1d6bd625fd5ece9ac588cf5d65e440044
j.dammann <j.dammann@stud.uni-goettingen.de>
2024-08-18 04:01:18 +0200

    test.py added

---
+++ test.py
@@ -0,0 +1,39 @@
+import torch
+from utils import save_checkpoint, load_checkpoint, save_some_examples
+import torch.nn as nn
+import torch.optim as optim
+import config
+from dataset import Dataset
+from generator_model import Generator
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+
+torch.backends.cudnn.benchmark = True
+
+folder = "test"
+n = 10
+
+def main():
+    gen = Generator(in_channels=1, out_channels=106, features=64).to(config.DEVICE)
+    opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.5, 0.999))
+
+    load_checkpoint(config.CHECKPOINT_GEN, gen, opt_gen, config.LEARNING_RATE,)
+
+    val_dataset = Dataset(root_dir_x=config.VAL_DIR_X, root_dir_y=config.VAL_DIR_Y)
+    val_loader = DataLoader(val_dataset, batch_size=n, shuffle=False)
+
+    for i in range(n):
+        x, y = next(iter(val_loader))
+        x, y = x.to(config.DEVICE), y.to(config.DEVICE)
+        gen.eval()
+        with torch.no_grad():
+            y_fake = gen(x)
+            y_fake = y_fake * 0.5 + 0.5  # remove normalization#
+            save_image(y_fake, folder + f"/tl_gen_{i}.tif")
+            save_image(x * 0.5 + 0.5, folder + f"/tl_raw_{i}.tif")
+            save_image(y * 0.5 + 0.5, folder + f"/cb_raw_{i}.tif")
+    gen.train()
+
+
+if __name__ == "__main__":
+    main()
